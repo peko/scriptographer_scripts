191765ff1e4ae79cc72bf620a039dda85e7dcb9d
Vladimir Seregin <peko@gasubasu.com>
2015-01-21 18:45:04 +0400

    Tinny images changes api, again
    Added an downloader for images, script now drops a lot of images in script's folder

--- Splitter/ReplaceWithImage.js
+++ Splitter/ReplaceWithImage.js
@@ -1,4 +1,4 @@
-// Replace With An Image v.1.1
+// Replace With An Image v.1.2
 
 // Online search engine has some changes
 // So I fix request and parsing
@@ -30,9 +30,12 @@ importPackage(java.io);
 var bg = 'FFFFFF';
 var bg_w  = 0.25; // weight if bg color
 var hex_w = 0.75; // weight of main color
+var size = 100;
+
 
 items = document.selectedItems.clone();
 for (si in items) {
+
     var item = items[si]; 
     var color = item.fillColor;
     var R = (Math.round(color.red  *255)).toString(16); if (R.length<2) R='0'+R;
@@ -42,27 +45,42 @@ for (si in items) {
 
     if (item && hex) {
         
-        // OLD URL FORMAT
-        //var url = new URL('http://labs.ideeinc.com/coloursearch/services/rest/?method=color.search&quantity=50&page=0&colors='+bg+'%2C'+hex+'&imageset=flickr');
+
         
-        // NEW URL FORMAT (weights was added)
-        // http://labs.ideeinc.com/rest/?method=flickr_color_search&limit=60&offset=0&colors%5B0%5D=e84b33&colors%5B1%5D=48a85b&weights%5B0%5D=0.5&weights%5B1%5D=0.5
+        // NEW URL
+        // http://labs.tineye.com/multicolr/rest/color_search/
+        //     ?limit=73
+        //     &offset=0
+        //     &return_metadata=
+        //        %3CuserID%2F%3E
+        //        %3CphotoID%2F%3E
+        //        %3CimageWidth%2F%3E
+        //        %3CimageHeight%2F%3E
+        //     &colors%5B0%5D=e7402d
+        //     &weights%5B0%5D=100
         
-        // %5B & %5D it's just codes for [ & ]
+
+        
         var url = new URL(
-            'http://labs.ideeinc.com/rest/?'+
-            'method=flickr_color_search&'+
+            'http://labs.tineye.com/multicolr/rest/color_search/?'+
             'limit=50&'+
             'offset=0&'+
+            'return_metadata='+
+            '%3CuserID%2F%3E'+
+            '%3CphotoID%2F%3E'+
+            '%3CimageWidth%2F%3E'+
+            '%3CimageHeight%2F%3E&'+
             'colors%5B' +1+'%5D='+bg   +'&'+
             'colors%5B' +0+'%5D='+hex  +'&'+
             'weights%5B'+1+'%5D='+bg_w +'&'+
             'weights%5B'+0+'%5D='+hex_w
         );
         
+
         var stream = new DataInputStream(url.openStream());
         var json = '';
         var line;
+        
         while ((line = stream.readLine()) != null) {
             json += line;
         };
@@ -73,17 +91,36 @@ for (si in items) {
         } else {
             
             // RETURNIG DATA FORMAT LOOKS LIKE NEXT OBJECT: 
-            // .filepath:   "http://img.tineye.com/l...ic-files/2386476346.jpg"
-            // .id:         "2386476346"
-            // .photosite:  "http://www.flickr.com/p...46425925@N00/2386476346"
-            // .score:      "99.90"
+
+            // NEW URL
+            // http://img.tineye.com/flickr-images/?filepath=labs-flickr-public/images/
+            //     49/7584251048_4900a64b9b_m.jpg
+            //     &size=76
+
+            var file = data.result[Math.floor(Math.random()*50)]['filepath'];
+            var name = file.split('/')[1];
+            var path = "http://img.tineye.com/flickr-images/?size=" + size + "&filepath=labs-flickr-public/images/" + file;
+            print(path);
+
+            url = new java.net.URL(path);
             
-            url = new java.net.URL(data.result[Math.floor(Math.random()*50)]['filepath']); 
-            var image = new PlacedFile(url);
+            var input = new java.io.BufferedInputStream(url.openStream());
+            var output = new java.io.FileOutputStream(name);
+            var data = [];
+            var b = 0
+            while ((b = input.read()) != -1) {
+                output.write(b);
+            }
+            input.close()
+            output.close()
+            var img = new java.io.File("./"+name);
+            var image = new PlacedFile(img);
             image.bounds = item.bounds;
             var group = new Group();
+            
             group.appendChild(image);
             group.appendChild(item);
+            
             group.clipped = true;
             item.clipMask = true; 
         }
